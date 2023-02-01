6dd8833f50a9f423e432608a81603665e88f6861
Shackal <Cer.97@yandex.ru>
2023-02-01 21:01:30 +0300

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import Other.Bot;
 
 public class Main {
 
-    private static final String BOT_TOKEN = "5445585320:AAHh5U1OHS4UhzMXi2nFpJXT1GWuEzif6qE";
+    private static final String BOT_TOKEN = "";
 
     public static void main(String[] args) {
         Bot application = Bot.builder()
